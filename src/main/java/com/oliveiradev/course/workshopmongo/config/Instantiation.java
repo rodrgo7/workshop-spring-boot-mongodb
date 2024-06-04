b3181a19b0feec017069aeed7e98c9a032e853de
Rodrigo Oliveira <r.oliveira.95hotmail.com>
2024-06-04 18:26:36 -0300

    Add comments to posts

--- src/main/java/com/oliveiradev/course/workshopmongo/config/Instantiation.java
+++ src/main/java/com/oliveiradev/course/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.oliveiradev.course.workshopmongo.domain.Post;
 import com.oliveiradev.course.workshopmongo.domain.User;
 import com.oliveiradev.course.workshopmongo.dto.AuthorDTO;
+import com.oliveiradev.course.workshopmongo.dto.CommenDTO;
 import com.oliveiradev.course.workshopmongo.repository.PostRepository;
 import com.oliveiradev.course.workshopmongo.repository.UserRepository;
 
@@ -39,10 +40,19 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, sdf.parse("01/01/2024"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(zenitsu));
         Post post2 = new Post(null, sdf.parse("05/01/2024"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(zenitsu));
 
+        CommenDTO c1 = new CommenDTO("Boa viagem!", sdf.parse("06/01/2024"), new AuthorDTO(tanjiro));
+        CommenDTO c2 = new CommenDTO("Aproveite", sdf.parse("06/01/2024"), new AuthorDTO(tanjiro));
+        CommenDTO c3 = new CommenDTO("Venha para o Okinawa", sdf.parse("06/01/2024"), new AuthorDTO(yuji));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
+        
         userRepository.saveAll(Arrays.asList(tanjiro, yuji, zenitsu));
         postRepository.saveAll(Arrays.asList(post1, post2));
 
         zenitsu.getPosts().addAll(Arrays.asList(post1, post2));
         userRepository.save(zenitsu);
+
+
     }
 }

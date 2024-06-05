f340ae93ef51e8896c4932d81eb155437bd851cd
Rodrigo Oliveira <r.oliveira.95hotmail.com>
2024-06-05 10:01:42 -0300

    Query with any criterion

--- src/main/java/com/oliveiradev/course/workshopmongo/resources/PostResource.java
+++ src/main/java/com/oliveiradev/course/workshopmongo/resources/PostResource.java
@@ -6,6 +6,7 @@ import com.oliveiradev.course.workshopmongo.domain.Post;
 import com.oliveiradev.course.workshopmongo.resources.util.URL;
 import com.oliveiradev.course.workshopmongo.services.PostService;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,5 +36,17 @@ public class PostResource {
         text = URL.decodeParam(text);
         List<Post> list = service.findByTitle(text);
         return ResponseEntity.ok(list);
-    }    
+    }
+
+    @RequestMapping(value="/fullSearch", method=RequestMethod.GET)
+        public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value="text", defaultValue="") String text,
+            @RequestParam(value="minDate", defaultValue="") String minDate,
+            @RequestParam(value="maxDate", defaultValue="") String maxDate) {
+                text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = service.fullSearch(text, min, max);
+        return ResponseEntity.ok().body(list);
+        }
 }
--- src/main/java/com/oliveiradev/course/workshopmongo/services/PostService.java
+++ src/main/java/com/oliveiradev/course/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.oliveiradev.course.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,4 +27,9 @@ public class PostService {
         //return repo.findByTitleContainingIgnoreCase(text);
         return repo.searchTitle(text);
     }    
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000); // Data do prox. dia!
+        return repo.fullSearch(text, minDate, maxDate);
+    }
 }
